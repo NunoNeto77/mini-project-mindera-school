7c55d82d10398c5459ff7b796ac5d90dbd36c99c
Nuno Neto <73204727+NunoNeto77@users.noreply.github.com>
2022-01-03 21:01:57 +0000

    Update Music.java

--- src/listenToMusic/Music.java
+++ src/listenToMusic/Music.java
@@ -4,7 +4,6 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 import java.io.File;
 
-// clip é um tubo fechado
 
 public class Music {
 
@@ -15,11 +14,11 @@ public class Music {
 
             if (musicPath.exists()) {
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-                Clip clip = AudioSystem.getClip();  // Clip interface represents a special kind of data line whose audio data can be loaded prior to playback, instead of being streamed in real time
-                clip.open(audioInput); // Opens the clip with the format and audio data present in the provided audio input stream.
+                Clip clip = AudioSystem.getClip();  
+                clip.open(audioInput); 
                 clip.start();
 
-                clip.loop(Clip.LOOP_CONTINUOUSLY); // Java Clip LOOP_CONTINUOUSLY A value indicating that looping should continue indefinitely rather than complete after a specific number of loops.
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
 
                 JOptionPane.showMessageDialog(null, "hit ok to pause"); // when you supply a null argument like that, the JOptionPane dialog will be centered on the user’s screen.
                 long clipTimePosition = clip.getMicrosecondPosition();
@@ -29,7 +28,7 @@ public class Music {
                 clip.setMicrosecondPosition(clipTimePosition);
                 clip.start();
 
-                JOptionPane.showMessageDialog(null, "press ok to stop playing"); // impedir q a thread clip se mate
+                JOptionPane.showMessageDialog(null, "press ok to stop playing");
             }
 
             else {
